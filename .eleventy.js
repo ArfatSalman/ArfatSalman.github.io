999b8040c7e5a5e3ddf4caa65824f1d1e5262dfd
Arfat Salman <giney.paradise@gmail.com>
2021-06-24 11:38:15 +0530

    change folder structure & add under construction icon

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,34 @@
+const htmlmin = require('html-minifier');
+
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addWatchTarget('*.css');
-  eleventyConfig.addPassthroughCopy('*.css');
-  eleventyConfig.addPassthroughCopy('*.ico');
-  eleventyConfig.addPassthroughCopy('assets');
+  eleventyConfig.addWatchTarget('src/*.css');
+
+  const filesToCopy = ['src/*.css', 'src/*.ico', 'assets', 'CNAME'];
+  for (const fileURL of filesToCopy) {
+    eleventyConfig.addPassthroughCopy(fileURL);
+  }
+
+  eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
+    // Eleventy 1.0+: use this.inputPath and this.outputPath instead
+    if (outputPath && outputPath.endsWith('.html')) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+      return minified;
+    }
+
+    return content;
+  });
+
   return {
     passthroughFileCopy: true,
+    templateFormats: ['njk', 'md'],
+    htmlTemplateEngine: 'njk',
+    markdownTemplateEngine: 'njk',
+    dir: {
+      input: 'src',
+    },
   };
 };
